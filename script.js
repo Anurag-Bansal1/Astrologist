a5d8aa230833589ab1f6ccb5d6a8f3e24bf529e9
Anurag-Bansal1 <98122455+Anurag-Bansal1@users.noreply.github.com>
2022-01-21 14:44:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function playAudio() { document.getElementById("prank-audio").play();}
+
+function ghostimage() { document.getElementById("Ghost").style.backgroundImage = "Ghost.gif";}
+
+function RemoveItself(elem) { elem.parentNode.removeChild(elem);}
+
+function pranktext(){ document.getElementById("text-change").innerHTML = "<br><p style='font-weight:bold;color:red;font-size:70px;'>Your Computer Got Virus!</p>";}
