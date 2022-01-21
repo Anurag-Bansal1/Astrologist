d06c622c1d05b1085d38e75944fa6883d46ba0bd
Anurag-Bansal1 <98122455+Anurag-Bansal1@users.noreply.github.com>
2022-01-21 14:43:41 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function playAudio() { document.getElementById("myAudio").play();}
-
-function ghostimage() { document.getElementById("Ghost").style.backgroundImage = "Ghost.gif";}
-
-function RemoveItself(elem) { elem.parentNode.removeChild(elem);}
-
-function pranktext(){ document.getElementById("demo").innerHTML = "<br><p style='font-weight:bold;color:red;font-size:70px;'>Your Computer Got Virus!</p>";}
